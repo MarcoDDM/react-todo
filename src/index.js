df7400e35d7b4553f6c66cd1597b0af812414867
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-05-08 14:32:06 -0300

    Fix lntrs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import TodoApp from './components/TodoApp';
 import reportWebVitals from './reportWebVitals';
 
